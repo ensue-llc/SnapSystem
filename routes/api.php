df5202699482ef290d79dc73655b16d53b99f2d9
Shiva Thapa <shiva.thapa@ensue.us>
2023-06-19 18:40:32 +0545

    Api fix

--- routes/api.php
+++ routes/api.php
@@ -3,13 +3,6 @@
 use Ensue\Snap\Controllers\FileController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/upload-resources', [
-    'use'=>'FileController@uploadResources',
-    'as' =>'upload-resources',
-    'middleware' => 'auth:api']);
-
-Route::post('/upload-resources/multiple', [
-    'use'=>'FileController@uploadMultipleResources',
-    'as' =>'upload-resources',
-    'middleware' => 'auth:api']);
+Route::post('/upload-resources', [FileController::class,'uploadResources'])->middleware('auth:api');
+Route::post('/upload-resources/multiple', [FileController::class,'uploadMultipleResources'])->middleware('auth:api');
 
